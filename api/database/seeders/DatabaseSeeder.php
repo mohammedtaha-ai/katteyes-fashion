a3979a61309d337849831e76c5fe480691e83fae
mohammedtaha-ai <mohammedtaha-ai@users.noreply.github.com>
2026-09-18 03:13:42 +0300

    feat(api): CategorySeeder for 6 default categories

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([AdminSeeder::class]);
+        $this->call([
+            AdminSeeder::class,
+            CategorySeeder::class,
+        ]);
     }
 }
